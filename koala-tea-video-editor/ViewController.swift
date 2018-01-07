8a25e725c6165bbd4d92ea5ddec5befc3ae55d37
Craig Holliday <themisterholliday@gmail.com>
2018-01-07 13:22:43 -0600

    Initial Commit

---
+++ koala-tea-video-editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  koala-tea-video-editor
+//
+//  Created by Craig Holliday on 1/7/18.
+//  Copyright © 2018 Koala Tea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ koala-tea-video-editorTests/koala_tea_video_editorTests.swift
@@ -0,0 +1,36 @@
+//
+//  koala_tea_video_editorTests.swift
+//  koala-tea-video-editorTests
+//
+//  Created by Craig Holliday on 1/7/18.
+//  Copyright © 2018 Koala Tea. All rights reserved.
+//
+
+import XCTest
+@testable import koala_tea_video_editor
+
+class koala_tea_video_editorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
